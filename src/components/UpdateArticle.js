3e35e85ce4586fb621e6963b8ccd9a1e020969eb
YUNA9627 <yuna2175@naver.com>
2024-10-28 16:19:30 +0900

    edit update

--- src/components/UpdateArticle.js
+++ src/components/UpdateArticle.js
@@ -3,12 +3,11 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 export default class UpdateArticle extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       title : this.props.data.title,
-      desc : this.props.data.desc
+      desc:this.props.data.desc
     }
   }
   modifyValue = (e)=>{
@@ -17,55 +16,33 @@ export default class UpdateArticle extends Component {
     })
   }
 
+
+
   render() {
     return (
       <section>
-        <article>
-          <h2>Update Article</h2>
-          <Form onSubmit={(e)=>{
-            e.preventDefault();
-            this.props.onsubmit(
-              this.state.title,
-              this.state.title
-            );
-          }}>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Title</Form.Label>
-              <Form.Control 
-              value={this.state.title}
-              // value는 readonly로 적용됨 defaultValue는 수정 가능
-              onChange={(e)=>{
-                this.setState({
-                  title:e.target.value
-                })
-              }}
-              onChange={this.modifyValue}
-              type="text" 
-              name="title" 
-              placeholder="Title" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Description</Form.Label>
-              <Form.Control 
-              value={this.state.desc}
-              // value는 readonly로 적용됨 defaultValue는 수정 가능
-              onChange={(e)=>{
-                this.setState({
-                  desc:e.target.value
-                })
-              }}
-              onChange={this.modifyValue}
-              as="textarea" 
-              name="desc" 
-              rows={3} />
-            </Form.Group>
-            <Button 
-            type="submit" 
-            variant="success"
-            >Submit</Button>
-          </Form>
-        </article>
-      </section>
+          <article>
+              <h2>Update Article</h2>
+              <Form onSubmit={(e)=>{
+                e.preventDefault();         
+                this.props.onsubmit(
+                  this.state.title,
+                  this.state.desc
+                );
+              }}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control type="text" name="title" placeholder="title" value={this.state.title} onChange={this.modifyValue}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control as="textarea" name="desc" rows={3} value={this.state.desc} 
+                  onChange={this.modifyValue}/>
+                </Form.Group>
+                <Button type="submit" variant="primary">submit</Button>
+              </Form>
+          </article>
+      </section> 
     )
   }
 }
